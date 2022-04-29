c2a8e8c0a1a88ced6759455052dea00ee97c89f5
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-04-30 00:32:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Copyright © 2022 Jonathan Sanfilippo
+#  2022 Jonathan Sanfilippo
 
 
 
 #set archlinux grub theme 
-sudo cp -r archlinux-grub /boot/grub/themes/
+sudo cp -r arch /boot/grub/themes/
 sudo mv /etc/default/grub /etc/default/grub.bak
 sudo cp -r grub /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
